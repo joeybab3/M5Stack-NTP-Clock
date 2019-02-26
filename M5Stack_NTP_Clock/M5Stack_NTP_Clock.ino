80747eb681621d8310b21a8f79999d71f2d4a174
joeybab3 <joey_babcock@yahoo.com>
2019-02-25 23:53:26 -0800

    added date and time stuff

--- M5Stack_NTP_Clock/M5Stack_NTP_Clock.ino
+++ M5Stack_NTP_Clock/M5Stack_NTP_Clock.ino
@@ -6,7 +6,11 @@
 const char* ssid     = "NETGEAR45";
 const char* password = "orangeflower393";
 
-#define NTP_OFFSET  0 // In seconds 
+String formattedDate;
+String dayStamp;
+String timeStamp;
+
+#define NTP_OFFSET  -28797 // In seconds for los angeles/san francisco time zone
 #define NTP_INTERVAL 60 * 1000    // In miliseconds
 #define NTP_ADDRESS  "pool.ntp.org"
 
@@ -33,8 +37,8 @@ void setup(void) {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
-  delay(500);
-  Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected.");
@@ -47,8 +51,66 @@ void setup(void) {
 void loop() {
   timeClient.update();
   String formattedTime = timeClient.getFormattedTime();
+  formattedDate = timeClient.getFormattedDate();
+  int splitT = formattedDate.indexOf("T");
+  
+  String dateStamp = formattedDate.substring(0, splitT);
+  String dayStamp = formattedDate.substring(splitT-2, splitT);
+  String monthStamp = formattedDate.substring(splitT-5, splitT-3);
+  String yearStamp = formattedDate.substring(0, 4);
+  String yearStampSmall = formattedDate.substring(2, 4);
+  String timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  String hourStamp = timeStamp.substring(0,2);
+  String minuteStamp = timeStamp.substring(3,5);
+  String secondStamp = timeStamp.substring(6,8);
+
+  int year = yearStamp.toInt();
+  int month = monthStamp.toInt();
+  int day = dayStamp.toInt();
+  int hour = hourStamp.toInt();
+  int minute = minuteStamp.toInt();
+  int second = secondStamp.toInt();
+
+  boolean isPm = false;
+  
+  int hourNoMilitary;
+  
+  if(hour > 12)
+  {
+    hourNoMilitary = hour - 12;
+    isPm = true;
+  }
+  else
+  {
+    isPm = false;
+    hourNoMilitary = hour;
+  }
+  String hourStampNoMilitary = (String)hourNoMilitary;
+  if(hourNoMilitary < 10)
+  {
+    hourStampNoMilitary = "0"+hourNoMilitary;
+  }
+
+  int daysLeft = 0;
+  int monthsLeft = 0;
+
+  String timeStampNoMilitary = hourStampNoMilitary + ":" + minuteStamp + ":" + secondStamp;    //hh:mm:ss
+  String dateStampConstructed = monthStamp + "/" + dayStamp + "/" + yearStampSmall;  //mm/dd/yy
+  
+  M5.Lcd.setTextSize(6);
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setCursor(10, 10);
-  M5.Lcd.print(formattedTime);
-  delay(1000);
+  M5.Lcd.setCursor(0, 10);
+  M5.Lcd.print(dateStampConstructed);
+  M5.Lcd.println(timeStampNoMilitary);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(290, 60);
+  if(isPm)
+  {
+    M5.Lcd.println("PM");
+  }
+  else
+  {
+    M5.Lcd.println("AM");
+  }
+  delay(950);
 }
